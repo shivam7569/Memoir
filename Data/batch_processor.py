51729e4ce919d2b110d4955905b81a23ef120e84
Shivam <shivam.iitmandi@gmail.com>
2020-04-11 12:58:19 +0530

    Update batch_processor.py

--- Data/batch_processor.py
+++ Data/batch_processor.py
@@ -45,9 +45,9 @@ def path_maker(v_type, srs):
     return path
 
 
-def batch_generator(images, batch_size=64, image_size=(576, 384)):
+def batch_generator(image_names, batch_size=64, image_size=(576, 384)):
     '''
-    images: List of names of images for a given video_type and series.
+    image_names: List of names of images for a given video_type and series.
     batch_size: Size of the batch of images to make.
     image_size: Final size of the returned images.
     '''
@@ -56,12 +56,12 @@ def batch_generator(images, batch_size=64, image_size=(576, 384)):
 
     batch = []  # list to store images of batch
     random_indices = np.random.randint(
-        low=0, high=len(images), size=batch_size)  # Generating random indices to pick images
+        low=0, high=len(image_names), size=batch_size)  # Generating random indices to pick images
     for ind in random_indices:
 
         # Reading and storing the images according to the random indices generated, after resizing them.
 
-        image_name = images[ind]
+        image_name = image_names[ind]
 
         img = cv2.imread(image_name)  # Reading an image.
         img = cv2.resize(img, image_size)  # Resizing the image.
